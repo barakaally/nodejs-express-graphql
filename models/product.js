a4804be8423ae0e0be7175deb1b9a977426d3f9e
barakaally <baraka.ally.co.tz@gmail.com>
2020-02-02 20:58:43 +0300

    added product model

---
+++ models/product.js
@@ -0,0 +1,15 @@
+
+class Product {
+
+    constructor() {
+  /**@type {String} */  this.name;
+  /**@type {String} */  this.description;
+  /**@type {String} */  this.location;
+  /**@type {Boolean} */  this.isDelivable;
+  /**@type {Number} */  this.avalablePieces;
+  /**@type {Number} */  this.remainPieces;
+    }
+
+}
+
+module.exports = Product;
